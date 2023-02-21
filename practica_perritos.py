5edd9d2928a9f73370ebb30ed150583e7ea9213a
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-02-20 22:54:40 -0600

    coreccion

--- practica_perritos.py
+++ practica_perritos.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from pandas import pandas as pd
 
+#Leemos el csv con la información
 dogData = pd.read_csv('muestra_perros.csv')
 
+#Inicialización de la interfaz.
 root = tk.Tk()
 root.title('Iterface')
 root.geometry('600x350')
 
-dogAgesFrame = pd.DataFrame(dogData)
-dogAgesGraphic = plt.figure(figsize=(6, 3), dpi = 100)
-
-ax1 = dogAgesGraphic.add_subplot(111)
-bar1 = FigureCanvasTkAgg(dogAgesGraphic, root)
+#Gráfica de la edad de los perros:
+dogFrame = pd.DataFrame(dogData)
+dogGraphic = plt.figure(figsize=(6, 3), dpi = 100)
+ax1 = dogGraphic.add_subplot(111)
+bar1 = FigureCanvasTkAgg(dogGraphic, root)
 bar1.get_tk_widget().pack(side = tk.LEFT, fill = tk.NONE)
-
-dogAgesFrame = dogAgesFrame[['edad']].round(decimals = 0).value_counts().sort_index()
-dogAgesFrame.plot(kind='bar', legend=False, ax = ax1)
-
-ax1.set_title('Psserros por edad')
+dogFrame = dogFrame[['edad']].round(decimals = 0).value_counts().sort_index()
+dogFrame.plot(kind='bar', legend=False, ax = ax1)
+ax1.set_title('Perros por edad')
 
 
 root.mainloop()
